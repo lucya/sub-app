71892691f0bc6d4fa952ea26116640fdc6551c40
lucya <neddang@gmail.com>
2024-06-15 22:10:14 +0900

    test

---
+++ src/AnotherReactApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AnotherReactApp = () => {
+  return (
+    <div>
+      <h2>Another React App</h2>
+      {/* 해당 애플리케이션의 UI */}
+    </div>
+  );
+};
+
+export default AnotherReactApp;
